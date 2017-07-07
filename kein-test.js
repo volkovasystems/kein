ddb2703f5b90ead47fbcfa8656ad7847077a8e81
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-07 14:42:39 +0800

    Updated server/client test.

--- kein-test.js
+++ kein-test.js
@@ -2,15 +2,13 @@
 const assert = require( "assert" );
 const kein = require( "./kein.js" );
 
-//assert.equal( kein( "toString", null ), true, "should be true" );
-
 assert.equal( kein( "toString", NaN ), true, "should be true" );
 
 assert.equal( kein( "toString", undefined ), true, "should be true" );
 
 assert.equal( kein( "toString", 123 ), true, "should be true" );
 
-assert.equal( kein( 123, { 123: "hello" } ), true, "should return true" );
+assert.equal( kein( "toString", ( ) => { } ), true, "should be true" );
 
 assert.equal( kein( Symbol.for( "property" ), { [ Symbol.for( "property" ) ]: "value" } ), true, "should be true" );
 

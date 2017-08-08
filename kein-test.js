21130411ae2e92add5689a81d5c3e22c00fecd39
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-08 13:47:07 +0800

    Updated test.

--- kein-test.js
+++ kein-test.js
@@ -2,16 +2,16 @@
 const assert = require( "assert" );
 const kein = require( "./kein.js" );
 
-assert.equal( kein( "toString", NaN ), true, "should be true" );
+assert.equal( kein( "toString", NaN ), true, "should be equal to true" );
 
-assert.equal( kein( "toString", undefined ), true, "should be true" );
+assert.equal( kein( "toString", undefined ), true, "should be equal to true" );
 
-assert.equal( kein( "toString", 123 ), true, "should be true" );
+assert.equal( kein( "toString", 123 ), true, "should be equal to true" );
 
-assert.equal( kein( "toString", ( ) => { } ), true, "should be true" );
+assert.equal( kein( "toString", ( ) => { } ), true, "should be equal to true" );
 
-assert.equal( kein( Symbol.for( "property" ), { [ Symbol.for( "property" ) ]: "value" } ), true, "should be true" );
+assert.equal( kein( Symbol.for( "property" ), { [ Symbol.for( "property" ) ]: "value" } ), true, "should be equal to true" );
 
-assert.equal( kein( "property", { "property": "value" } ), true, "should be true" );
+assert.equal( kein( "property", { "property": "value" } ), true, "should be equal to true" );
 
 console.log( "ok" );
